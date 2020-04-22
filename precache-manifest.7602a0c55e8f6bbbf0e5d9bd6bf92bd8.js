a6d0763c9de8b0d8a7b78359da4a47d4d4581550
YusufKer <53224370+YusufKer@users.noreply.github.com>
2020-04-22 13:46:33 +0200

    Updates

---
+++ precache-manifest.7602a0c55e8f6bbbf0e5d9bd6bf92bd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1514418684d5e4a608b995f37bdfde",
+    "url": "/open-weather-app/index.html"
+  },
+  {
+    "revision": "554422b22ea523062522",
+    "url": "/open-weather-app/static/css/main.8b0205ec.chunk.css"
+  },
+  {
+    "revision": "5b1b723cead133fcf8e4",
+    "url": "/open-weather-app/static/js/2.1577c460.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/open-weather-app/static/js/2.1577c460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554422b22ea523062522",
+    "url": "/open-weather-app/static/js/main.e3146733.chunk.js"
+  },
+  {
+    "revision": "6b57a76bb2029d5829d5",
+    "url": "/open-weather-app/static/js/runtime-main.6555a28c.js"
+  },
+  {
+    "revision": "f80dde903fe81a042e8aa9f23b072859",
+    "url": "/open-weather-app/static/media/background.f80dde90.jpeg"
+  }
+]);
